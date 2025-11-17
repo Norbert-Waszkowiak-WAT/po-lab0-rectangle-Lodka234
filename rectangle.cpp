5e70b21370c2b96d473b9cee124b94bab5bef561
Lodka234 <spitfire1@op.pl>
2025-11-17 10:04:28 +0000

    zadanie 0

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
-
+using namespace std;
 class Rectangle {
-
+private:
+    double width;
+    double hight;
+public:
+    rectangle(double w, double h):
+    width(w),hight(h){};
+    void setWidth(double h){
+        width = w;
+    }
+    void setHeight(double h){
+        hight = h;
+    }
+    double getWidth() const{
+        return height;
+    }
+    double area() const{
+        return width*hight;
+    }
+    double perimeter()const{
+        return 2*(hight+width);
+    }
+    double disply() const{
+        cout << "Rectangle:" << width << "x" << height << "h" << endl;
+        cout << "Area" << area() << endl;
+        cout << "Perimeter" << perimeter() << endl; 
+    }
 };
